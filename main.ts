5ef32adbf1718a604a6bc08ff8248b4d1e4f8faf
Masterkiller25 <98768627+Masterkiller25@users.noreply.github.com>
2022-04-13 21:43:06 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 OLED.init(128, 64)
+OLED.newLine()
+OLED.newLine()
+OLED.drawLoading(64)
 basic.forever(function () {
-    OLED.writeStringNewLine("" + sonar.ping(
+    OLED.writeNumNewLine(sonar.ping(
     DigitalPin.P15,
     DigitalPin.P14,
     PingUnit.Centimeters
     ))
-    basic.pause(100)
+    basic.pause(1000)
     OLED.clear()
 })
