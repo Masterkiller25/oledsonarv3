9bc8ff25a1ec09d41750c947b7ddc72c2845ad7f
Masterkiller25 <98768627+Masterkiller25@users.noreply.github.com>
2022-04-14 09:17:44 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
+let barnbr = 0
 OLED.init(128, 64)
+while (barnbr < 100) {
+    OLED.drawLoading(barnbr)
+    barnbr += 1
+}
+OLED.init(64, 128)
 OLED.newLine()
 OLED.newLine()
-OLED.drawLoading(64)
 basic.forever(function () {
-    OLED.writeNumNewLine(sonar.ping(
-    DigitalPin.P15,
-    DigitalPin.P14,
-    PingUnit.Centimeters
-    ))
-    basic.pause(1000)
+    while (input.buttonIsPressed(Button.A)) {
+        OLED.drawLoading(sonar.ping(
+        DigitalPin.P15,
+        DigitalPin.P14,
+        PingUnit.Centimeters
+        ) / 4)
+    }
     OLED.clear()
 })
