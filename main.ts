f730a785684df2da0578f70898ec055e45f52b92
Masterkiller25 <98768627+Masterkiller25@users.noreply.github.com>
2022-04-14 09:48:14 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,25 @@
 let barnbr = 0
 OLED.init(128, 64)
+OLED12864_I2C.init(120)
 while (barnbr < 100) {
     OLED.drawLoading(barnbr)
     barnbr += 1
 }
-OLED.init(64, 128)
+OLED.init(128, 64)
 OLED.newLine()
 OLED.newLine()
 basic.forever(function () {
     while (input.buttonIsPressed(Button.A)) {
-        OLED.drawLoading(sonar.ping(
+        OLED12864_I2C.showNumber(
+        58,
+        28,
+        sonar.ping(
         DigitalPin.P15,
         DigitalPin.P14,
         PingUnit.Centimeters
-        ) / 4)
+        ),
+        2
+        )
     }
     OLED.clear()
 })
